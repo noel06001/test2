4aa3b12f639d9242b7ded04603516dea6f6ab8ab
noell <noell@DESKTOP-NLQ10I7>
2023-10-08 02:09:12 +0200

    Add project files.

---
+++ test2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace test_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad heter Sveriges huvudstad?");
+            string x = Console.ReadLine().ToLower();
+            
+            while (x != "stockholm")
+            {
+               
+                if (x == "stokholm" || x == "stocholm")
+                {
+                    Console.WriteLine("Nära, försök igen");
+                    x = Console.ReadLine().ToLower();
+                }
+                else
+                {
+                    Console.WriteLine("Fel, försök igen");
+                    x = Console.ReadLine().ToLower();
+
+                }
+            }
+            
+            
+                
+            
+            Console.WriteLine("Grattis! Du gissade rätt.");
+
+            Console.ReadKey();
+        }
+    }
+}
